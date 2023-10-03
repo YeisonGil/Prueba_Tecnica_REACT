4320946777d243a2520e11426d6f429e7a4cf06c
Yeison Gil <yeisongil.a9701@gmail.com>
2023-10-03 08:16:54 -0500

    feat: :sparkles: Terminado el header para mobile y desktop

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/error' element={<h1>page error</h1>} />X
+          <Route path='/loading' element={<h1>page loading</h1>} />X
+          <Route path='/home' element={<h1>page home</h1>} />X
+          <Route path='/movies' element={<h1>page movies</h1>} />X
+          <Route path='/series' element={<h1>page series</h1>} />X
+        </Routes>
+        {/* <Footer/> */}
+      </BrowserRouter>
     </>
   )
 }
